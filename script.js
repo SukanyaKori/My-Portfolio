30289e2eda3aee2ab6012b824aa73f2b3022a292
Sukanya Kori <100477760+SukanyaKori@users.noreply.github.com>
2022-02-27 21:50:13 +0530

    My website
    It's my own website.

---
+++ script.js
@@ -0,0 +1,34 @@
+let container = document.querySelector('.container');
+
+let small = document.querySelector('small');
+
+let hr = document.querySelector('hr');
+
+let p = document.querySelector('p');
+
+let button = document.querySelector('.button');
+
+let button1 = document.querySelector('.button1');
+
+let icons = document.querySelector('.icons');
+
+setTimeout(() => {
+    small.style.visibility = 'visible';
+}, 2200);
+
+setTimeout(() => {
+    hr.style.visibility = 'visible';
+}, 3200);
+
+setTimeout(() => {
+    p.style.visibility = 'visible';
+}, 4200);
+
+
+setTimeout(() => {
+    icons.style.visibility = 'visible';
+    button.style.visibility = 'visible';
+    button1.style.visibility = 'visible';
+
+}, 5200);
+
